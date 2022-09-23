9d55baab5377d4787d05a253a3d1ac7e02b48211
zzaiyan <zzaiyan@outlook.com>
2022-09-23 22:12:40 +0800

    9/23

---
+++ jxw/EditDistance.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int leastEdit(string string1, string string2) {
+#define max(a, b) ((a) >= (b) ? (a) : (b))
+#define min(a, b) ((a) <= (b) ? (a) : (b))
+
+  int m = string1.size(), n = string2.size();
+  // vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+  int** dp = new int*[m + 1];
+  for (int i = 0; i <= m; i++)
+    dp[i] = new int[n + 1];
+
+  for (int i = 0; i <= m; i++)
+    dp[i][0] = i;
+  for (int i = 0; i <= n; i++)
+    dp[0][i] = i;
+
+  for (int i = 1; i <= m; i++) {
+    for (int j = 1; j <= n; j++) {
+      // 添加 和 删除
+      dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + 1;
+      // 替换 和 do nothing
+      dp[i][j] =
+          min(dp[i][j], dp[i - 1][j - 1] + (string1[i - 1] != string2[j - 1]));
+    }
+  }
+  int ret = dp[m][n];
+  for (int i = 0; i <= m; i++)
+    delete[] dp[i];
+  delete[] dp;
+  return ret;
+}
+
+int main() {
+  string s1, s2;
+
+  cin >> s1 >> s2;
+  cout << leastEdit(s1, s2);
+
+  return 0;
+}
--- jxw/kmp.cpp
+++ jxw/kmp.cpp
@@ -1,73 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int M = 1e6 + 10, N = 1e5 + 10;
+int bfCnt = 0, kmpCnt = 0;
 
-// void kmp(int str_size, char* str, int pat_size, char* pat) {
-//   int* ne = new int[pat_size + 5];
+int bf(const string& str, const string& pat) {
+  for (int i = 0; i < str.size(); i++) {
+    int k = i, j = 0;
+    while (k < str.size() && j < pat.size()) {
+      if (str[k] != pat[j])
+        break;
+      k++, j++, bfCnt++;
+    }
+    bfCnt++;  // 记录退出前的一次比较
+    if (j == pat.size())
+      return i;
+  }
+  return -1;
+}
 
-//   for (int i = 2, j = 0; i <= pat_size; i++) {
-//     while (j > 0 && pat[i] != pat[j + 1])
-//       j = ne[j];
-//     pat[i] == pat[j + 1] && j++;
-//     ne[i] = j;
-//   }
-
-//   cout << "\nne: ";
-//   for (int i = 1; i <= pat_size; i++)
-//     cout << ne[i] << ' ';
-//   cout << "\n";
-
-//   for (int i = 1, j = 0; i <= str_size; i++) {
-//     while (j > 0 && str[i] != pat[j + 1])
-//       j = ne[j];
-//     str[i] == pat[j + 1] && j++;
-//     if (j == pat_size)
-//       cout << "匹配成功: " << i - pat_size + 1 << endl;
-//   }
-
-//   delete[] ne;
-// }
-
-void kmp(const string& str, const string& pat) {
+int kmp(const string& str, const string& pat) {
   int* ne = new int[pat.size() + 2]{-1, 0};
 
   for (int i = 2; i < pat.size() + 1; i++) {
     int j = ne[i - 1];
     while (j && pat[i - 1] != pat[j])
-      // p[i] 与 P[j] 不同且 j 不为 0，就一直更新 j 为next[j]。
-      j = ne[j];  //询问上帝
+      j = ne[j];
     if (pat[i - 1] == pat[j])
       j++;
     ne[i] = j;
   }
 
-  cout << "\nne: ";
-  for (int i = 0; i < pat.size(); i++)
-    cout << ne[i] << ' ';
-  cout << "\n";
+  // cout << "\nne: ";
+  // for (int i = 0; i < pat.size(); i++)
+  //   cout << ne[i] << ' ';
+  // cout << "\n";
 
   for (int i = 0, j = 0; i < str.size(); i++) {
+    kmpCnt++;
     while (j && str[i] != pat[j])
       j = ne[j];
     if (str[i] == pat[j])
       j++;
     if (j == pat.size()) {
-      cout << "匹配成功: " << i - j + 1 << "\n";
+      // cout << "匹配成功: " << i - j + 1 << "\n";
+      return (i - j + 1);
     }
   }
   delete[] ne;
+  return -1;  // 匹配失败
 }
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0), cout.tie(0);
-  // char pat[N], str[M];
-  // int str_size, pat_size;
-  // cin >> str_size >> str + 1 >> pat_size >> pat + 1;
-  // kmp(str_size, str, pat_size, pat);
+
   string str, pat;
   cin >> str >> pat;
-  kmp(str, pat);
+
+  cout << bf(str, pat) << endl << bfCnt << endl;
+  cout << kmp(str, pat) << endl << kmpCnt << endl;
+
   return 0;
 }
