c8dd10219ebf9613287a428fd05dfeb3409779c1
zzaiyan <zzaiyan@outlook.com>
2022-10-08 23:52:04 +0800

    update sth

--- test.cpp
+++ test.cpp
@@ -6,6 +6,10 @@ int main() {
   int v = __cplusplus;
   printf("std = c++%d%d\n", v / 1000 % 10, v / 100 % 10);
   cout << v << endl;
-
+  for (int i = 0; i < 10; i++) {
+    cout << "(str[i*10 + " << i + 1 << "] - '0') * ";
+    cout << (1 << i);
+    cout << " + ";
+  }
   return 0;
 }
