abd3e01268bba377f96e03bdafd3be5b33908d61
zzaiyan <zzaiyan@outlook.com>
2022-11-13 09:51:07 +0800

    update

--- Model/01背包.cpp
+++ Model/01背包.cpp
@@ -1,18 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define MAXN 10005
 
-int N,V,dp[MAXN],v[MAXN],w[MAXN];
+constexpr int MAXN = 10005;
+int N, V, dp[MAXN], v[MAXN], w[MAXN];
 
-int main(){
-	cin>>N>>V;
-	for(int i=1;i<=N;i++)
-		scanf("%d%d",w+i,v+i);
-	
-	for (int i = 1; i <= N; ++i)
-		for (int j = V; j >= w[i]; --j)
-			dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
-	
-	printf("%d",dp[V]);
-	return 0;
+int main() {
+  scanf("%d%d", &N, &V);
+  for (int i = 1; i <= N; i++)
+    scanf("%d%d", w + i, v + i);
+
+  for (int i = 1; i <= N; ++i)
+    for (int j = V; j >= w[i]; --j)
+      dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+
+  printf("%d", dp[V]);
+  return 0;
 }
