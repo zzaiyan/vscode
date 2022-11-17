0c24a1d4de24faf5b468000be4b7421744e54d46
zzaiyan <zzaiyan@outlook.com>
2022-11-17 09:51:07 +0800

    update

--- ListTest.cpp
+++ ListTest.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 struct MyInt {
   int val;
-  bool operator<(const MyInt& A) {
+  bool operator<(const MyInt &A) {
     if (val < A.val) {
       cout << val << " < " << A.val << "\n";
     } else
       cout << val << " >= " << A.val << "\n";
     return val < A.val;
   }
-  const MyInt& operator=(const MyInt& A) {
+  const MyInt &operator=(const MyInt &A) {
     val = A.val;
     cout << "operator= calls. val = " << val << " .\n";
     return *this;
   }
-  MyInt operator-(const MyInt& A) { return MyInt{val - A.val}; }
+  MyInt operator-(const MyInt &A) { return MyInt{val - A.val}; }
 };
 
-void show(const list<MyInt>& L) {
+void show(const list<MyInt> &L) {
   for (auto i : L)
     cout << i.val << ' ';
   cout << endl;
@@ -39,7 +39,7 @@ int main() {
 
   cout << "L.begin() = " << (*p).val << endl;
 
-  L.sort();  // stl自带排序
+  L.sort(); // stl自带排序
 
   cout << "L.begin() = " << (*p).val << endl;
 
