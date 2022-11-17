553ee1e99362e63d2ed46ea5e96eced8a82ace71
zzaiyan <zzaiyan@outlook.com>
2022-11-17 19:51:07 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,22 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    printf("Hello iPhone!\n");
-    int v = __cplusplus;
-    printf("std = c++%d%d\n", v / 1000 % 10, v / 100 % 10);
-    cout << v << endl;
+int main() {
+  printf("Hello iPhone!\n");
+  int v = __cplusplus;
+  printf("std = c++%d%d\n", v / 1000 % 10, v / 100 % 10);
+  cout << v << endl;
 
-    string line, out;
-    while (getline(cin, line))
-    {
-        if (line != "end")
-            out.append("\"" + line + "\"\n");
-        else
-            break;
-    }
-    cout << "\n"
-         << out << "\n";
-    return 0;
+  string line, out;
+  while (getline(cin, line)) {
+    if (line != "end")
+      out.append("\"" + line + "\"\n");
+    else
+      break;
+  }
+  cout << "\n" << out << "\n";
+  return 0;
 }
