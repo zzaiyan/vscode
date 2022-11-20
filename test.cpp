90cd8db5960e4fe50df4f8b399e25adbd887c9f4
Zhang Zaiyan <94966504+zzaiyan@users.noreply.github.com>
2022-11-20 06:31:40 +0000

    update

--- test.cpp
+++ test.cpp
@@ -1,19 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
   printf("Hello iPhone!\n");
   int v = __cplusplus;
   printf("std = c++%d%d\n", v / 1000 % 10, v / 100 % 10);
-  cout << v << endl;
-
-  string line, out;
-  while (getline(cin, line)) {
-    if (line != "end")
-      out.append("\"" + line + "\"\n");
-    else
-      break;
-  }
-  cout << "\n" << out << "\n";
+  // cout << v << endl;
   return 0;
 }
