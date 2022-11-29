92d4f053177aa99063d79bc443443933263fd9f5
zzaiyan <zzaiyan@outlook.com>
2022-11-29 22:38:57 +0800

    update

---
+++ 算法基础课/第一讲/快速排序.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[1000005];
+
+template <class T> void quicksort(T *a, int l, int r) {
+  // if(l>=r)return;
+  int x = a[l + r >> 1], i = l, j = r;
+  do {
+    while (a[i] < x)
+      i++;
+    while (a[j] > x)
+      j--;
+    if (i <= j)
+      swap(a[i++], a[j--]);
+  } while (i <= j);
+  if (l < j)
+    quicksort(a, l, j);
+  if (i < r)
+    quicksort(a, i, r);
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++)
+    scanf("%d", arr + i);
+  quicksort(arr, 1, n);
+  for (int i = 1; i <= n; i++) {
+    if (i - 1)
+      putchar(' ');
+    printf("%d", arr[i]);
+  }
+  return 0;
+}
