cb015de5e133e0cfc2d82045041a0875c5d92cd7
zzaiyan <zzaiyan@outlook.com>
2023-01-04 21:16:05 +0800

    udpate

--- test.cpp
+++ test.cpp
@@ -20,23 +20,23 @@ auto test() {
 
 int main() {
   test();
-  printf("Hello iPhone!\n");
-  int v = __cplusplus;
-  printf("std = c++%d%d\n", v / 1000 % 10, v / 100 % 10);
-  cout << v << endl;
+  // printf("Hello iPhone!\n");
+  // int v = __cplusplus;
+  // printf("std = c++%d%d\n", v / 1000 % 10, v / 100 % 10);
+  // cout << v << endl;
 
-  string inBuf;
+  // string inBuf;
 
-  set<string> st;
-  while (cin >> inBuf) {
-    if (inBuf.size() <= 1)
-      break;
-    st.insert(inBuf);
-  }
-  int cnt = 0;
-  for (auto &&i : st) {
-    cout << i << ", " << cnt++ << endl;
-  }
+  // set<string> st;
+  // while (cin >> inBuf) {
+  //   if (inBuf.size() <= 1)
+  //     break;
+  //   st.insert(inBuf);
+  // }
+  // int cnt = 0;
+  // for (auto &&i : st) {
+  //   cout << i << ", " << cnt++ << endl;
+  // }
 
   return 0;
 }
